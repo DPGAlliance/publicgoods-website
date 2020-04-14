8d3a5cd51c4ad6c4101f9476713296750a0a004b
Victor Grau Serrat <lacabra@users.noreply.github.com>
2020-04-14 16:40:23 -0600

    BLD: add React filters of nominees

---
+++ explore/precache-manifest.7c89b7a5b04e4c7c1c52cf6367fbfeb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b67b967b8d67389db7a205794d80a706",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98d9cad6d7c621414001",
+    "url": "/static/css/main.95cb2b5b.chunk.css"
+  },
+  {
+    "revision": "21059e97b36dfb5ed85d",
+    "url": "/static/js/2.2a74f637.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.2a74f637.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98d9cad6d7c621414001",
+    "url": "/static/js/main.804bb8f6.chunk.js"
+  },
+  {
+    "revision": "e85aa2da7cde43d4c0c8",
+    "url": "/static/js/runtime-main.f89d26cc.js"
+  }
+]);
